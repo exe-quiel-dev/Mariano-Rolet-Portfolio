8daf009af75cad8716913c6563abd09f287c3574
Exequiel-Dev <exe.quiel.developer@oulook.com>
2023-01-18 08:01:42 -0300

    Change languages updated

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+const en = document.querySelector('#lang-en');
+
+en.addEventListener('click', langEn);
+
+function langEn() {
+    location.href = 'indexEn.html'
+}
---
+++ Js/indexEn.js
@@ -0,0 +1,7 @@
+const es = document.querySelector('#lang-es');
+
+es.addEventListener('click', langEs);
+
+function langEs() {
+    location.href = 'index.html'
+}
